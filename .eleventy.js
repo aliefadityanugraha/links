c4af878b7757dc86e09e5d8792f881b8122ab3ee
aliefadityanugraha <80080471+aliefadityanugraha@users.noreply.github.com>
2023-05-13 11:27:50 +0700

    fresh installation

--- .eleventy.js
+++ .eleventy.js
@@ -4,20 +4,16 @@ const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const htmlmin = require("html-minifier");
 
 module.exports = function (eleventyConfig) {
-  // Disable automatic use of your .gitignore
   eleventyConfig.setUseGitIgnore(false);
 
-  // Merge data instead of overriding
   eleventyConfig.setDataDeepMerge(true);
 
-  // human readable date
   eleventyConfig.addFilter("readableDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
       "dd LLL yyyy"
     );
   });
 
-  // Syntax Highlighting for Code blocks
   eleventyConfig.addPlugin(syntaxHighlight);
 
   // To Support .yaml Extension in _data
---
+++ readme.md
@@ -0,0 +1,1 @@
+https://github.com/altertek/whatimupto
